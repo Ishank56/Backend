02659ac09504a5e2b545feb279cf20f70d0ab813
Ishank56 <92422560+Ishank56@users.noreply.github.com>
2024-08-25 12:41:35 +0530

    Add files via upload

---
+++ bajaj-backend-master/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.post('/bfhl', (req, res) => {
+    const data = req.body.data;
+    const numbers = [];
+    const alphabets = [];
+    let highestLowerCase = null;
+
+    data.forEach(item => {
+        if (!isNaN(item)) {
+            numbers.push(item);
+        } else if (typeof item === 'string') {
+            alphabets.push(item);
+            if (item === item.toLowerCase() && (!highestLowerCase || item > highestLowerCase)) {
+                highestLowerCase = item;
+            }
+        }
+    });
+
+    const response = {
+        is_success: true,
+        user_id: "ishank_goel_28062003",
+        email: "Deepali56!",
+        roll_number: "21BEC0435",
+        numbers: numbers,
+        alphabets: alphabets,
+        highest_lowercase_alphabet: highestLowerCase ? [highestLowerCase] : []
+    };
+    res.status(200).json(response);
+});
+
+app.get('/bfhl', (req, res) => {
+    res.status(200).json({ operation_code: 1 });
+});
+
+const port = process.env.PORT || 9000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
